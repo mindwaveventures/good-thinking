c19584cc753eebbbb1e906f0d469086352535fd1
Sam Houston <shouston3@me.com>
2017-05-05 12:57:54 +0100

    adding lookingfor section

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.fields import TextField
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
@@ -9,11 +10,13 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    lookingfor = RichTextField(blank=True)
     alphatext = RichTextField(blank=True)
     footer = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        FieldPanel('lookingfor', classname="full"),
         FieldPanel('alphatext', classname="full"),
         FieldPanel('footer', classname="full"),
     ]
