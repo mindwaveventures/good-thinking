aa88075b7bf9d67a204e29ac677df199b52a6047
Danielwhyte <danwhy@gmail.com>
2017-07-18 14:21:13 +0100

    implements new structure for homepage/resources #57, #51

--- resources/models.py
+++ resources/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.db.models.fields import TextField, URLField, IntegerField
+from django.db.models.fields import TextField, URLField, IntegerField, CharField
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
@@ -9,6 +9,63 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
+
+from wagtail.wagtailimages.models import Image
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+
+from django.template.loader import get_template
+
+from itertools import chain
+
+class Home(Page):
+    banner = RichTextField(blank=True, help_text="Banner at the top of every page")
+    header = RichTextField(blank=True, help_text="Hero title")
+    body = RichTextField(blank=True, help_text="Description of page")
+    filter_label_1 = TextField(blank=True, help_text="Label/Question for first set of filters")
+    filter_label_2 = TextField(blank=True, help_text="Label/Question for second set of filters")
+    filter_label_3 = TextField(blank=True, help_text="Label/Question for third set of filters")
+    assessment_text = RichTextField(blank=True, help_text="Label for sleep assessment link")
+    crisis_text = RichTextField(blank=True, help_text="Label for sleep crisis page link")
+    lookingfor = RichTextField(blank=True, help_text="Information on how to leave suggestions and what the suggestions are for")
+    alpha = RichTextField(blank=True, help_text="What is Alpha")
+    alphatext = RichTextField(blank=True, help_text="Why to take part in the alpha")
+    footer = RichTextField(blank=True, help_text="Footer text")
+    hero_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Max file size: 10MB. Choose from: GIF, JPEG, PNG (but pick PNG if you have the choice)"
+    )
+    video_url = URLField(blank=True, help_text="URL of an introductiary youtube video")
+
+    def get_context(self, request):
+        # Update contest to inclue only published posts, ordered by revers-chron
+        context = super(Home, self).get_context(request)
+        resource_index_page = self.get_children()
+        for e in resource_index_page:
+            if (e.title == 'Resource Index'):
+                resources = map(combine_tags, e.get_children().live().order_by('-first_published_at'))
+        context['resources'] = resources
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('banner', classname="full"),
+        ImageChooserPanel('hero_image'),
+        FieldPanel('header', classname="full"),
+        FieldPanel('body', classname="full"),
+        FieldPanel('video_url', classname="full"),
+        FieldPanel('filter_label_1', classname="full"),
+        FieldPanel('filter_label_2', classname="full"),
+        FieldPanel('filter_label_3', classname="full"),
+        FieldPanel('assessment_text', classname="full"),
+        FieldPanel('crisis_text', classname="full"),
+        FieldPanel('lookingfor', classname="full"),
+        FieldPanel('alpha', classname="full"),
+        FieldPanel('alphatext', classname="full"),
+        FieldPanel('footer', classname="full"),
+    ]
 
 class TopicTag(TaggedItemBase):
     content_object = ParentalKey('resources.ResourcePage', related_name='tagged_topic_items')
@@ -105,3 +162,12 @@ class ResourcePage(Page):
 
     class Meta:
         verbose_name = "Resource"
+
+def combine_tags(element):
+    element.specific.tags = list(chain(
+        element.specific.content_tags.all(),
+        element.specific.issue_tags.all(),
+        element.specific.reason_tags.all(),
+        element.specific.topic_tags.all()
+    ))
+    return element
