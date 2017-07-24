5ec0c8b82c1742eeb25eb90381f4cbe5112c3409
Danielwhyte <danwhy@gmail.com>
2017-07-24 13:36:37 +0100

    adds static pages with streamfield #65

--- static/models.py
+++ static/models.py
@@ -2,14 +2,34 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.db.models.fields import CharField
+
+from wagtail.wagtailcore import blocks
 from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.fields import RichTextField, StreamField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class StaticPage(Page):
-    body = RichTextField(blank=True)
+    body = StreamField([
+        ('heading', blocks.RichTextBlock()),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
+
+    ALIGNMENT_CHOICES = (
+      ('tl', 'left'),
+      ('tc', 'center')
+    )
+    text_alignment = CharField(
+      choices=ALIGNMENT_CHOICES,
+      max_length=10,
+      default='left',
+      help_text='Text Alignment'
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        FieldPanel('text_alignment'),
+        StreamFieldPanel('body'),
     ]
