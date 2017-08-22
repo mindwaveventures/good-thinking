fe91350447ad90d48853911ebb62fcb86d5ae82b
Danielwhyte <danwhy@gmail.com>
2017-08-22 11:51:32 +0100

    fixes url path for full resource #157

--- resources/models/resources.py
+++ resources/models/resources.py
@@ -116,6 +116,10 @@ class ResourcePage(Page):
         FieldPanel('priority'),
     ]
 
+    def __init__(self, *args, **kwargs):
+        super(ResourcePage, self).__init__(*args, **kwargs)
+        self.parent = self.get_parent().slug
+
     def get_context(self, request):
         context = super(ResourcePage, self).get_context(request)
 
