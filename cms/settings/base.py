71bbce4ca73737870f4e9fecb5c53683f508be1b
Danielwhyte <danwhy@gmail.com>
2017-10-05 12:36:59 +0100

    adds template cache loader

--- cms/settings/base.py
+++ cms/settings/base.py
@@ -95,13 +95,18 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(PROJECT_DIR, 'templates'),
         ],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
             ],
         },
     },
