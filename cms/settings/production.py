9857eb1cde0a3ff156860416fd1d717276b9f00c
David Bower <david@mindwaveventures.com>
2017-10-13 12:59:27 +0100

    nginx config - set server name and force HTTPS only access.  Production settings - set secure host/cookie settings unless environment variable set to permit HTTP access (this can then be used when building up a deployment, ie testing with HTTP access before establishing HTTPS)

--- cms/settings/production.py
+++ cms/settings/production.py
@@ -15,15 +15,17 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# Taken from https://www.reddit.com/r/Heroku/comments/51um8g/forcing_https_redirect/
-# SECURE_SSL_REDIRECT = True
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+if not 'NON_HTTPS_ACCESS_OKAY' in os.environ:
+    # If NON_HTTPS_ACCESS_OKAY environment variable is *not* set, then force HTTPS access only
+    # Taken from https://www.reddit.com/r/Heroku/comments/51um8g/forcing_https_redirect/
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_HSTS_SECONDS = 31536000
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-# SECURE_BROWSER_XSS_FILTER = True
-# CSRF_COOKIE_HTTPONLY = True
-# X_FRAME_OPTIONS = 'DENY'
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    CSRF_COOKIE_HTTPONLY = True
+    X_FRAME_OPTIONS = 'DENY'
