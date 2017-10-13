724e70a1d9876a4dc8001f6df7fee13a40498949
Danielwhyte <danwhy@gmail.com>
2017-10-13 15:28:21 +0100

    adds validation on submitting feedback #649

--- cms/static/js/feedback.js
+++ cms/static/js/feedback.js
@@ -96,7 +96,7 @@ function delete_visited(id) {
 }
 
 var delete_cookie = function(name) {
-  document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
 };
 
 feedbackLoopListener();
