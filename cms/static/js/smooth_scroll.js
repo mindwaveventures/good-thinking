36ec3cfc36cb3d071aabc4bb1ea04d428d6853b5
katbow <katherine.bowler@gmail.com>
2017-10-25 11:44:43 +0100

    Refactor to reuse for smooth scrolling elsewhere #692 #415

--- cms/static/js/smooth_scroll.js
+++ cms/static/js/smooth_scroll.js
@@ -1,20 +1,24 @@
+function scrollToTarget(target) {
+  $('html, body').animate({
+    scrollTop: target.offset().top
+  }, function() {
+    var $target = $(target);
+    $target.focus();
+    if ($target.is(":focus")) {
+      return false;
+    } else {
+      $target.attr('tabindex','-1');
+      $target.focus();
+    };
+  });
+}
+
 $('#browse-arrow')
   .click(function(e) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
       e.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, function() {
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) {
-          return false;
-        } else {
-          $target.attr('tabindex','-1');
-          $target.focus();
-        };
-      });
+      scrollToTarget(target);
     }
   });
