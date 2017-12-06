a783bc6c9b691c8592606043f404bdd47d09ab00
David Bower <david@mindwaveventures.com>
2017-12-06 13:56:26 +0000

    E24 self assessment - add a finish/homepage button to the end of the assessment and direct user's to the site's home page.

--- resources/views.py
+++ resources/views.py
@@ -489,6 +489,7 @@ def assessment_controller(self, request, **kwargs):
     context['resources'] = resources
     context['heading'] = self.heading
     context['body'] = self.body
+    context['finish_destination'] = "/"
 
     if (self.seo_title):
         context['page_title'] = (
