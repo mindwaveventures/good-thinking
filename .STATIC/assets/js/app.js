985a81ad5c00e02392b4071286fff3e108de5db3
priyanka parthiban <priyanka@sippaibot.com>
2018-05-14 17:56:45 +0530

    static design update 28

--- .STATIC/assets/js/app.js
+++ .STATIC/assets/js/app.js
@@ -130,6 +130,37 @@ $( document ).ready(function() {
   });
 
 
-    
+    var gtswiperselfassessment = new Swiper('.gt-swiper-self-assessment', {
+    slidesPerView: 1.4,
+    spaceBetween: 10,
+    centeredSlides: true,
+    pagination: {
+      el: '.gt-self-assessment-swiper-pagination',
+      clickable: true,
+    },
+        navigation: {
+      nextEl: '.gt-arrow-rt',
+      prevEl: '.gt-arrow-lt',
+    },
+      breakpoints: {
+        1024: {
+          slidesPerView: 1.4,
+          spaceBetween:10,
+        },
+        990: {
+          slidesPerView: 1.1,
+          spaceBetween: 10,
+        },
+        800: {
+          slidesPerView: 1.1,
+          spaceBetween: 5,
+        },
+        320: {
+          slidesPerView: 1.1,
+          spaceBetween: 5,
+        }
+      }
+        
+  });
     
 });
