2f0d1be767f86ed8eaf6e9f02f5f279acec21884
priyanka <priyanka@sippaibot.com>
2019-01-23 17:25:35 +0530

    #222

--- static/models.py
+++ static/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from django.db.models import Q
 
 from django.db.models.fields import CharField
 from django.db import models
@@ -9,12 +10,19 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from resources.models.helpers import base_context
-
+from django.apps import apps
 
 class StaticPage(Page):
     def get_context(self, request):
         context = super(StaticPage, self).get_context(request)
 
+        Home = apps.get_model('resources', 'home')
+
+        for all_menu in Home.objects.all():
+            for sub_menu in all_menu.sub_menu.all():
+                if sub_menu.submenu_pagelink.url == request.path:
+                    context['matched_submenu'] = Home.objects.filter(Q(slug=all_menu.slug))
+        context['current_path'] = request.path
         return base_context(context,self)
 
     cover_image = models.ForeignKey(
